d9f36706bcecf331718ba7163d5745c1413ba31c
Terry Holmes <tchaser3@outlook.com>
2024-04-20 10:07:39 -0400

    Add project files.

---
+++ ViewPartNumbers/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NewEventLogDLL;
+using NewPartNumbersDLL;
+
+namespace ViewPartNumbers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //setting up the classes
+        EventLogClass TheEventLogClass = new EventLogClass();
+        PartNumberClass ThePartNumberClass = new PartNumberClass();
+        WPFMessagesClass TheMessagesClass = new WPFMessagesClass();
+
+        PartNumbersDataSet ThePartNumbersDataSet;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ThePartNumbersDataSet = ThePartNumberClass.GetPartNumbersInfo();
+
+                dgrParts.ItemsSource = ThePartNumbersDataSet.partnumbers;
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "View Part Numbers // Window Loaded " + Ex.Message);
+
+                TheMessagesClass.ErrorMessage(Ex.ToString());
+            }
+            
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            TheMessagesClass.CloseTheProgram();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
